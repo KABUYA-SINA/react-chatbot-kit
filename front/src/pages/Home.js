1fef4ba49c84e3c748be1dca94cd326c7369b0c9
KABUYA <kabuya-sina@netcourrier.com>
2023-09-27 18:23:24 +0200

    sass home + animations

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ChatbotBox from '../components/Chatbot';
 import { IoChatbubblesSharp } from 'react-icons/io5';
+import { motion } from "framer-motion"
 import 'react-chatbot-kit/build/main.css';
 import '../sass/base/_base.scss';
 import '../sass/pages/_home.scss';
@@ -14,14 +15,26 @@ const Home = () => {
         const parent = document.querySelector('#parent > div > div')
         parent.className = "child"
     });
+    const variants = {
+        open: { opacity: 1, x: 0 },
+        closed: { opacity: .9, x: "-100%" },
+    }
 
     return (
         <div className='home'>
             <Header />
             <main className='main'>
-                <button className='btn' type='button' onClick={() => toggleBot((prev) => !prev)}>
+                <motion.button
+                    className='btn'
+                    type='button'
+                    onClick={() => toggleBot((prev) => !prev)}
+                    animate={showBot ? "open" : "closed"}
+                    variants={variants}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                >
                     <IoChatbubblesSharp size='3rem' />
-                </button>
+                </motion.button>
                 {
                     showBot && (
                         <ChatbotBox />
